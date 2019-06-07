535957864d70372d719089189923b4a20061dbfc
Lutylu <thibaultlegerinti@gmail.com>
2019-06-07 17:03:31 +0200

    On a ingoré le node_modules

--- routes/films.js
+++ routes/films.js
@@ -1,84 +1,103 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var model = require('../models/index');
-
+var model = require("../models/index");
 
 /* GET home page. */
-router.get('/films', function(req, res, next) {
-	console.log("*****************************************")
-	model.Films.findAll({})
-	.then(films => res.json({
-		error: false,
-		data: films
-	}))
-	.catch(films => res.json({
-		error: true,
-		data: [],
-		error: error
-	}))
+router.get("/films", function(req, res, next) {
+  model.Films.findAll({})
+    .then(films =>
+      res.json({
+        error: false,
+        data: films
+      })
+    )
+    .catch(films =>
+      res.json({
+        error: true,
+        data: [],
+        error: error
+      })
+    );
 });
 
-router.post('/film', function(req, res, next) {
-	model.Films.create({
-		title: req.body.title,
-		description: req.body.description,
-		image: req.body.image,
-		url: req.body.url
-	})
-	.then(films => res.status(201).json({
-		error: false,
-		data: films,
-		message : "Film crée"
-	}))
-	.catch(films => res.status(401).json({
-		error: true,
-		data: [],
-		error: error
-	}))
+router.post("/film", function(req, res, next) {
+  console.log("*****************BORDEL*******************************");
+  model.Films.create({
+    title: req.body.title,
+    description: req.body.description,
+    image: req.body.image,
+    url: req.body.url
+  })
+    .then(films =>
+      res.status(201).json({
+        error: false,
+        data: films,
+        message: "Film crée"
+      })
+    )
+    .catch(films =>
+      res.status(401).json({
+        error: true,
+        data: [],
+        error: error
+      })
+    );
 });
 
-router.put('/film/:id', function(req, res, next) {
-	model.Films.update({
-		title: req.body.title,
-		description: req.body.description,
-		image: req.body.image,
-		url: req.body.url
-	},
-	{
-		where: {
-			id: req.params.id
-		}
-	})
-	.then(films => res.status(201).json({
-		error: false,
-		data: films,
-		message : "Film mis à jour"
-	}))
-	.catch(films => res.status(401).json({
-		error: true,
-		data: [],
-		error: error
-	}))
-
+router.put("/film/:id", function(req, res, next) {
+  model.Films.update(
+    {
+      title: req.body.title,
+      description: req.body.description,
+      image: req.body.image,
+      url: req.body.url
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(films =>
+      res.status(201).json({
+        error: false,
+        data: films,
+        message: "Film mis à jour"
+      })
+    )
+    .catch(films =>
+      res.status(401).json({
+        error: true,
+        data: [],
+        error: error
+      })
+    );
 });
 
-router.delete('/film/:id', function(req, res, next) {
-	console.log("****************************************************************", req.params.id);
-	model.Films.destroy({
-		where: {
-			id: req.params.id
-		}
-	})
-	.then(films => res.status(201).json({
-		error: false,
-		data: films,
-		message : "Film supprimé"
-	}))
-	.catch(films => res.status(401).json({
-		error: true,
-		data: [],
-		error: error
-	}))
+router.delete("/film/:id", function(req, res, next) {
+  console.log(
+    "****************************************************************",
+    req.params.id
+  );
+  model.Films.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(films =>
+      res.status(201).json({
+        error: false,
+        data: films,
+        message: "Film supprimé"
+      })
+    )
+    .catch(films =>
+      res.status(401).json({
+        error: true,
+        data: [],
+        error: error
+      })
+    );
 });
 
 module.exports = router;
